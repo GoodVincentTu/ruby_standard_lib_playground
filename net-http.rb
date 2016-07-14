3621a552e25b52f18cbf5fe4aa1a8fbf79d609c4
metatrontu@gmail.com <metatrontu@gmail.com>
2016-07-14 17:54:34 +0800

    play net-http module

---
+++ net-http.rb
@@ -0,0 +1,22 @@
+# 'cuz need to simulate the server connection
+# using $ruby -run -ehttpd . -p8080
+
+require 'net/http'
+require 'json'
+
+class Todo
+	attr_reader :title, :done
+
+	def self.all
+		response = Net::HTTP.get(URI("http://localhost:8080/todos.json"))
+		todos_list = JSON.parse(response)["todos"]
+		todos_list.map { |todo| Todo.new todo["title"], todo["done"] }
+	end
+
+	def initialize title, done
+		@title = title
+		@done = done
+	end
+end
+
+puts Todo.all.inspect
